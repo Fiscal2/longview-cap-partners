2025b8fb97d5a15aee646e2f63a330a42934fdaa
Fiscal2 <zacharyross3@gmail.com>
2022-07-30 14:13:47 -0400

    navbar + appropriate routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 const routes: Routes = [
 { path: 'home', component: HomeComponent },
 { path: '', redirectTo: '/home', pathMatch: 'full' },
+{ path: 'About Us', component: NavbarComponent  },
+{ path: 'Contact', component: NavbarComponent },
 ];
 
 @NgModule({
