5b38b9c23ddee82c7632e9ea743ceb7454434963
Alphabetsoup16 <81780117+Alphabetsoup16@users.noreply.github.com>
2022-08-02 15:55:00 -0400

    ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
-import { NavbarComponent } from './navbar/navbar.component';
 
 const routes: Routes = [
-{ path: 'home', component: HomeComponent },
-{ path: '', redirectTo: '/home', pathMatch: 'full' },
-{ path: 'About Us', component: NavbarComponent },
-{ path: 'aboutus', pathMatch: 'full' },
-{ path: 'Contact', component: ContactComponent  },
-{ path: 'contact', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'contact', component: ContactComponent },
 ];
 
 @NgModule({
