e47e8ba13812cec0c55bab00b093c819ef850aa2
Alphabetsoup16 <81780117+Alphabetsoup16@users.noreply.github.com>
2022-08-08 23:36:41 -0400

    look

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -14,13 +14,21 @@ export class ContactComponent implements OnInit {
   }
 
   SendEmail() {
-    const form = document.querySelector("form") as HTMLInputElement | null;
-    form?.addEventListener("submit", (event) => {
-      // const { name, email, company, message } = event.target as HTMLInputElement;
-      event.preventDefault();
-      const emailTarget = event.target as HTMLInputElement;
-      console.log(event)
-      console.log(emailTarget.value)
-    });
+    const form = document.querySelector("form") as HTMLFormElement;
+    form.onsubmit = () => {
+      const formData = new FormData(form);
+
+      const text = formData.get('name') as string;
+      console.log(text);
+      console.log(formData);
+      return false;
+    };
+    // form?.addEventListener("submit", (event) => {
+    //   // const { name, email, company, message } = event.target as HTMLInputElement;
+    //   event.preventDefault();
+    //   const emailTarget = event.target;
+    //   console.log(event)
+    //   console.log(emailTarget)
+    // });
   }
 }
