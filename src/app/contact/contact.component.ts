3e70b11820bc04a869cbde0f84a07590d342900d
Fiscal2 <zacharyross3@gmail.com>
2022-08-10 14:05:51 -0400

    liveAlert stuff

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -14,6 +14,34 @@ export class ContactComponent implements OnInit {
   }
 
   SendEmail(formDataToEmail: any) {
-    this.emailSender.emailBuilder(formDataToEmail).subscribe(response => console.log(response));
+    this.emailSender.emailBuilder(formDataToEmail).subscribe(response => {
+
+
+      const alertPlaceholder = document.getElementById('liveAlertPlaceholder');
+                        
+      const alert = (message: any, type: any) => {
+          const wrapper = document.createElement('div')
+          wrapper.innerHTML = [
+            `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+            `   <div>${message}</div>`,
+            '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+            '</div>'
+          ].join('')
+        
+          alertPlaceholder?.append(wrapper)
+        }
+
+        const alertTrigger = document.getElementById('liveAlertBtn')
+        if (response.status === 200) {
+          alertTrigger?.addEventListener('click', () => {
+          alert('Nice, you triggered this alert message!', 'success')
+      })
+    } else{
+      alertTrigger?.addEventListener('click', () => {
+        alert('Nice, you triggered this alert message!', 'danger')
+      })
+
+    }
+    });
   }
 }
