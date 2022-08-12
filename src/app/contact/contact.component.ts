9cca37ea831deb91e90b2dbaae7936d058438f99
Alphabetsoup16 <81780117+Alphabetsoup16@users.noreply.github.com>
2022-08-11 23:10:46 -0400

    wow

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,3 +1,4 @@
+import { HttpResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { EmailSenderService } from '../email-sender.service';
@@ -14,28 +15,52 @@ export class ContactComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  SendEmail(formDataToEmail: NgForm) {
-    this.emailSender.emailBuilder(formDataToEmail.value).subscribe((response) => {
-      const alertPlaceholder = document.getElementById('liveAlert');
 
-      const alert = (message: string, type: string) => {
-        const wrapper = document.createElement('div')
-        wrapper.innerHTML = [
-          `<div class="alert alert-${type} alert-dismissible fade show" role="alert">`,
-          `   <div>${message}</div>`,
-          '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
-          '</div>'
-        ].join('')
-        alertPlaceholder?.append(wrapper)
-      }
+  OnSubmit(emailForm: NgForm) {
+    this.FormButton('on');
+    this.SendEmail(emailForm);
+  }
 
-      if (response.status === 200) {
-        alert('Email successfully sent!', 'success');
-      } else {
-        alert('Error sending email, Try Again!', 'danger');
-      }
-      formDataToEmail.resetForm();
-      console.log(formDataToEmail.onReset())
+  FormButton(status: string) {
+    const submitButton = document.getElementById('submitBtn');
+    const loadingSpinner = document.getElementById('loadSpin');
+    if (status == 'on') {
+      loadingSpinner?.classList.remove('d-none');
+      submitButton?.classList.add('disabled');
+    } else {
+      loadingSpinner?.classList.add('d-none');
+      submitButton?.classList.remove('disabled');
+    }
+  }
+
+  AlertBuilder(message: string, type: string) {
+    const alertPlaceholder = document.getElementById('liveAlert');
+    const wrapper = document.createElement('div')
+    wrapper.innerHTML = [
+      `<div class="alert alert-${type} alert-dismissible fade show" role="alert">`,
+      `   <div>${message}</div>`,
+      '   <button id="close" type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+      '</div>'
+    ].join('')
+    alertPlaceholder?.append(wrapper);
+  }
+
+  ShowAlert(form: NgForm, httpResponse: HttpResponse<any>) {
+    if (httpResponse.status === 200) {
+      this.AlertBuilder('Email successfully sent!', 'success');
+    } else {
+      this.AlertBuilder('Error sending email, Try Again!', 'danger');
+    }
+    form.resetForm();
+    setTimeout(() => {
+      document.getElementById("close")?.click();
+    }, 6000);
+  }
+
+  SendEmail(emailData: NgForm) {
+    this.emailSender.emailBuilder(emailData.value).subscribe((response) => {
+      this.ShowAlert(emailData, response);
+      this.FormButton('off');
     });
   }
 }
